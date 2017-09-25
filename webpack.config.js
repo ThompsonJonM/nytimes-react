40c257ae05fc1ce2fbd3890a5194abd46819fa8e
unknown <thompson.jonm@gmail.com>
2017-09-25 12:15:17 -0500

    webpack for react test done

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,53 @@
 // Webpack Config
 
 // Dependencies
+import path from 'path';
+import webpack from 'webpack';
+
+const buildDirectory = './dist/';
+
+export const webpackConfig = {
+    entry: './lib/main.jsx',
+
+    /* This will configure the dev server which helps
+    recompile script. */
+    devServer: {
+        hot: true,
+        inline: true,
+        port: 7700,
+        historyApiFallback: true,
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+    },
+
+    /* Output tells Webpack what to create using app script.
+    This will then be hosted on 7700. */
+    output: {
+        path: path.resolve(buildDirectory),
+        filename: 'app.js',
+        publicPath: 'http://localhost:7700/dist',
+    },
+
+    /* These are for Enzyme to ensure that it works properly
+    during the testing phase. */
+    externals: {
+        'cheerio': 'window',
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true,
+    },
+
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel',
+            query: {
+                presets: ['react', 'es2015', 'stage-0'],
+            },
+        }],
+    },
+
+    plugins: [],
+};
